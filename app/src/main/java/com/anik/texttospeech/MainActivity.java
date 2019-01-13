e921dba1588fec198ce8ad780d22ef4944f1a407
Noor Mohammed Anik <noormohammedanik@gmail.com>
2019-01-13 18:59:04 +0600

    initail update

---
+++ app/src/main/java/com/anik/texttospeech/MainActivity.java
@@ -0,0 +1,66 @@
+package com.anik.texttospeech;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText etxtText;
+    Button btnSpeak;
+    TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etxtText=findViewById(R.id.etxt_text);
+        btnSpeak=findViewById(R.id.btn_speak);
+        tts=new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+               if (status!=TextToSpeech.ERROR)
+               {
+                   tts.setLanguage(Locale.US);
+               }
+            }
+        });
+
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=etxtText.getText().toString();
+
+                tts.speak(text,TextToSpeech.QUEUE_FLUSH,null);
+                tts.setSpeechRate(1.0f);
+
+
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    protected void onPause() {
+
+        if (tts!=null)
+        {
+            //to stop speech
+            tts.stop();
+            tts.shutdown();
+
+        }
+        super.onPause();
+    }
+}
